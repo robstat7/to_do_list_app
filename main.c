daf86502d6fbea5619edeee8437d0729f4feaf48
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2024-10-17 17:36:19 +0530

    main.c: add delete task functionality

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <gtk/gtk.h>
 
+/* add a new task to the tasks box */
 static void add_task(gpointer *user_data, const gchar *task_text)
 {
 	GtkWidget *row, *hbox, *label, *check_button, *tasks_box;
@@ -17,6 +18,36 @@ static void add_task(gpointer *user_data, const gchar *task_text)
 
         gtk_list_box_insert(GTK_LIST_BOX(tasks_box), row, -1);
         gtk_widget_show_all(tasks_box);
+}
+
+static void on_delete_task_clicked(GtkWidget *widget, gpointer tasks_box)
+{
+	GtkWidget *dialog, *selected_row;
+	gint result;
+	const gchar *task_text;
+
+	/* create a dialog to delete selected task */
+	dialog = gtk_dialog_new_with_buttons("Delete selected task?",
+					     GTK_WINDOW(gtk_widget_get_toplevel(widget)),
+					     GTK_DIALOG_MODAL |
+					     GTK_DIALOG_DESTROY_WITH_PARENT,
+					     "_OK",
+					     GTK_RESPONSE_OK,
+					     "_Cancel",
+					     GTK_RESPONSE_CANCEL,
+					     NULL);
+
+	/* run the dialog and get user input */
+	result = gtk_dialog_run(GTK_DIALOG(dialog));
+
+	if(result == GTK_RESPONSE_OK) {
+			selected_row = (GtkWidget *) gtk_list_box_get_selected_row(
+					GTK_LIST_BOX(tasks_box));
+			/* destroy the row */
+			gtk_widget_destroy(selected_row);
+	}
+
+	gtk_widget_destroy(dialog);
 }
 
 static void on_add_task_clicked(GtkWidget *widget, gpointer tasks_box)
@@ -59,8 +90,9 @@ static void on_add_task_clicked(GtkWidget *widget, gpointer tasks_box)
 static void activate(GtkApplication *app, gpointer user_data)
 {
 	GtkApplication *application;
-	GtkWidget *win, *today_label, *vbox, *toolbar, *add_task_tool_img, *tasks_box;
-	GtkToolItem *add_task_button;
+	GtkWidget *win, *today_label, *vbox, *toolbar, *delete_task_tool_img,
+		  *add_task_tool_img, *tasks_box;
+	GtkToolItem *add_task_button, *delete_task_button;
 
 	/* create a new application window */
 	application = GTK_APPLICATION (app);
@@ -87,6 +119,15 @@ static void activate(GtkApplication *app, gpointer user_data)
 	/* set tooltip for the add task button */
     	gtk_widget_set_tooltip_text(GTK_WIDGET(add_task_button), "Add Task");
 
+	/* create delete task button */
+	delete_task_tool_img = gtk_image_new_from_file("resources/delete_task.png");
+    	delete_task_button = gtk_tool_button_new(delete_task_tool_img, "Delete Task");
+    	gtk_tool_button_set_label(GTK_TOOL_BUTTON(delete_task_button), "Delete Task");
+	gtk_toolbar_insert(GTK_TOOLBAR(toolbar), delete_task_button, -1);
+
+	/* set tooltip for the add task button */
+    	gtk_widget_set_tooltip_text(GTK_WIDGET(delete_task_button), "Delete Task");
+
 	/* add toolbar to the vertical box */
     	gtk_box_pack_start(GTK_BOX(vbox), toolbar, FALSE, FALSE, 0);
 
@@ -104,6 +145,9 @@ static void activate(GtkApplication *app, gpointer user_data)
 
 	/* connect the add task button to the callback function */
 	g_signal_connect(add_task_button, "clicked", G_CALLBACK(on_add_task_clicked), tasks_box);
+
+	/* connect the delete task button to the callback function */
+	g_signal_connect(delete_task_button, "clicked", G_CALLBACK(on_delete_task_clicked), tasks_box);
 
 	gtk_window_present(GTK_WINDOW (win));
 	gtk_widget_show_all(win);
