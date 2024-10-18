04be4e3a64cd4aee19cd60cbd960c802b6e66a75
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2024-10-18 11:03:29 +0530

    on_row_checkbox_toggled: find task id

--- checkbox.c
+++ checkbox.c
@@ -2,13 +2,13 @@
 
 void on_row_checkbox_toggled(GtkToggleButton *togglebutton, gpointer user_data)
 {
-	GtkWidget *row;
+	GtkWidget *hbox;
 
-	row = GTK_WIDGET(user_data);
+	hbox = GTK_WIDGET(user_data);
 
 	if (gtk_toggle_button_get_active(togglebutton)) {
-		g_print("@checkbox in row is checked\n");
+		g_print("@checkbox for task id %d is checked\n", *(gint *)g_object_get_data(G_OBJECT(hbox), "task_id"));
     } else {
-		g_print("@checkbox in row is unchecked\n");
+		g_print("@checkbox for task id %d is unchecked\n", *(gint *)g_object_get_data(G_OBJECT(hbox), "task_id"));
     }
 }
