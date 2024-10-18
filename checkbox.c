f0d2d0658bea64c7bbfc134a45d1d905ed438317
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2024-10-18 14:48:43 +0530

    update db checkbox status when unchecked

--- checkbox.c
+++ checkbox.c
@@ -7,15 +7,17 @@ void on_row_checkbox_toggled(GtkToggleButton *togglebutton, gpointer user_data)
 	short int task_id;
 
 	hbox = GTK_WIDGET(user_data);
+	task_id = *(gint *)g_object_get_data(G_OBJECT(hbox), "task_id");
 
 	if (gtk_toggle_button_get_active(togglebutton)) {
-		task_id = *(gint *)g_object_get_data(G_OBJECT(hbox), "task_id");
-
 		g_print("@checkbox for task id %d is checked\n", task_id);
 
 		/* update the checkbox status in the db */
 		db_update_checkbox(task_id, 1);
     	} else {
-		g_print("@checkbox for task id %d is unchecked\n", *(gint *)g_object_get_data(G_OBJECT(hbox), "task_id"));
+		g_print("@checkbox for task id %d is unchecked\n", task_id);
+	
+		/* update the checkbox status in the db */
+		db_update_checkbox(task_id, 0);
     	}
 }
